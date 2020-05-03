b1c3aad63bd07c842b31524b6d4d82437653ce1a
kapblam <nik.kaplan@gmail.com>
2020-05-02 22:09:30 -0700

    Added problem 10...takes a very long while...

--- euler.py
+++ euler.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 # problem 1 - Sum of Multiples of 3 and 5 up to a given n
@@ -176,19 +177,83 @@ def primefind2(n):
 
     return primes[n - 1]
 
+
 # Problem 8 - Largest product in given series using n adjacent digits
 def largeProductInSeries(n):
-    series = str("7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450")
+    series = str(
+        "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450")
     output = 1
     prod = 1
-    for i in range(len(series)-n):
+    for i in range(len(series) - n):
         for j in range(n):
-            prod = prod*int(series[j+i])
+            prod = prod * int(series[j + i])
         if prod > output:
             output = prod
         prod = 1
 
     return output
+
+
+# Problem 9: Special Pythagorean Triplet: there exists exactly one Pythagorean triplet for which
+# a+b+c=1000. Find the product abc.
+def problem9():
+    start = time.time()
+    cap = 500
+    for i in range(cap):
+        for j in range(i, cap, 1):
+            for k in range(j, cap, 1):
+                if i ** 2 + j ** 2 == k ** 2 and i + j + k == 1000:
+                    output = [i * j * k, i, j, k]
+
+    print("Runtime: %.7f" % (time.time() - start))
+    return output
+
+
+# Problem 10: Summation of Primes.  Find sum of all primes below 2 million:
+# note: takes a VERY long time to run, but the answer 142913828922 is correct!
+def problem10(n):
+    start = time.time()
+    # took from primefind2() above:
+    primes = [2]
+    attempt = 3
+    output = 0
+    while primes[-1] < n:
+        if all(attempt % x != 0 for x in primes):
+            primes.append(attempt)
+        attempt += 2
+    for i in range(len(primes) - 1):
+        output += primes[i]
+    print("Runtime: %.7f" % (time.time() - start))
+    return output
+
+
+# Problem 10 again, but let's try using the Sieve of Eratosthenes:  Does not finish in reasonable time
+# (waiting 1hr plus...)
+def sieveOfEratosthenes(n):
+    # Generate list of numbers from 2 to n.  Remember 1 is NOT a prime number
+    start = time.time()
+    nums = list()
+    rmv = list()
+    for i in range(2, n + 1):
+        nums.append(i)
+    # starting from 2 find all multiples then iterate the multiple until no multiples are left:
+    p = 2
+
+    while len(nums)-nums.index(p) > p in nums:
+        for i in nums:
+            if i != p and i % p == 0:
+                rmv.append(i)
+        for i in rmv:
+            nums.remove(i)
+        p = nums[nums.index(p)+1]
+        rmv = list()
+        sum = 0
+    for i in nums:
+        sum += i
+    print("Runtime: %.7f" % (time.time() - start))
+    return sum
+
+
 
 # print(mult35(1000))
 # print(fibsum(4000000)[1])
@@ -198,5 +263,8 @@ def largeProductInSeries(n):
 # print(sumSquare(100))
 # print(primeFindFact(10001))
 # print(primeFind(10001))
-# print(primefind2(10001))
-print(largeProductInSeries(13))
+# print(primefind2(20001))
+# print(largeProductInSeries(13))
+# print(problem9())
+# print(problem10(2000000))
+# print(sieveOfEratosthenes(2000000))
